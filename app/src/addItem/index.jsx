710ce621406fd726117a3a5eae7e5df173da4458
pravinbashyal <pravin.bashyal@gmail.com>
2016-05-19 22:57:28 +0545

    add todo list

--- app/src/addItem/index.jsx
+++ app/src/addItem/index.jsx
@@ -1,23 +1,56 @@
 import React from 'react';
 
 export default class extends React.Component {
+
+  handleTitleChange (e) {
+    console.log(this.state);
+    this.setState ({
+      title: e.target.value
+    });
+  }
+
+  handleTimeChange (e) {
+    console.log(this.state);
+    this.setState ({
+      time: e.target.value
+    });
+  }
+
+  handleDescriptionChange (e) {
+    console.log(this.state);
+    this.setState ({
+      description: e.target.value
+    })
+  }
+
+  submit () {
+    console.log(this.props.addItems);
+    this.props.addItems(this.state);
+    // console.log(this.state);
+  }
+
   render () {
     return (
       <div className="row">
         <div className="six columns">
           <label for="title">Title</label>
-          <input className="u-full-width" type="text" placeholder="Input Title" id="title" name="title"/>
+          <input className="u-full-width" type="text" placeholder="Input Title" id="title" name="title" onChange={this.handleTitleChange.bind(this)}/>
         </div>
         <div className="six columns">
           <label for="time">Time</label>
-          <input className="u-full-width" type="text" placeholder="HH:MM am/pm" id="time" name="time"/>
+          <input className="u-full-width" type="text" placeholder="HH:MM am/pm" id="time" name="time" onChange={this.handleTimeChange.bind(this)}/>
         </div>
         <div className="twelve columns">
           <label for="description">Description</label>
-          <textarea className="u-full-width" type="text" placeholder="Describe" id="description" name="description">
+          <textarea className="u-full-width" type="text" placeholder="Describe" id="description" name="description" onChange={this.handleDescriptionChange.bind(this)}>
           </textarea>
         </div>
-        <div className="twelve column no-margin">
+        <div className="two columns no-padding no-margin">
+          <input type="submit" 
+          onClick={this.submit.bind(this)}
+          />
+        </div>
+        <div className="twelve columns no-margin">
           <hr className="u-full-width"/>
         </div>
       </div>
--- app/src/app.jsx
+++ app/src/app.jsx
@@ -3,20 +3,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import FilteredList from "./filteredList/index.jsx";
-import AddItem from "./addItem/index.jsx"
+import AddItem from "./addItem/index.jsx";
+import Table from "./table/index.jsx";
 
 class App extends React.Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      items: []
+    };
+  }
+
+  addItems (item) {
+    var items = this.state.items;
+    item.archived = false;
+    items.push(item);
+    this.setState({
+      items: items
+    });
+    console.log('state', this.state);
+  }
+
   render () {
     return (
       <div className="app">
         <div class="row">
-          <h1 class="twelve columns center-text">
+          <h4 class="twelve columns center-text">
             Add Todo
-          </h1>
-          <AddItem/>
+          </h4>
+          <AddItem addItems={this.addItems.bind(this)}/>
         </div>
         <div class="row">
-          <FilteredList/>
+          <Table items={this.state.items}/>
         </div>
       </div>
     )
